40f756b824dda13d119640dca0cbf3d21049378f
TechnicPlay <33784577+TechnicPlay@users.noreply.github.com>
2019-06-19 17:24:35 +0200

    Updated Model

--- create_model.py
+++ create_model.py
@@ -4,8 +4,8 @@ def create_model():
     model = tf.keras.models.Sequential();
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
     model.add(tf.keras.layers.Dense(512, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1024, activation=tf.nn.relu))
     model.add(tf.keras.layers.Dense(256, activation=tf.nn.sigmoid))
 
     opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
